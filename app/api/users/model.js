c33fa53fe28d1a3d9679e2b4049288e3b6603c56
Justin D'Errico <justin.derrico1991@gmail.com>
2017-01-23 00:09:47 -0500

    added delete cascade for prototypes

--- app/api/users/model.js
+++ app/api/users/model.js
@@ -1,11 +1,19 @@
 import mongoose from 'mongoose';
 
+import Prototype from '../prototypes/model';
+
 const Schema = mongoose.Schema;
 
-export default mongoose.model('User', new Schema({
+const User = new Schema({
   email: {
     type: String,
     index: { unique: true },
   },
   password: String,
-}));
+});
+
+User.post('remove', (doc) => {
+  Prototype.remove({ user: doc._id }).exec();
+});
+
+export default mongoose.model('User', User);
