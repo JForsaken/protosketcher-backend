30e88c553a243299c1d4882b1710cf0635478dd2
Justin D'Errico <justin.derrico1991@gmail.com>
2017-02-21 14:14:57 -0500

    specify host and scheme to swagger

--- app/config/apidocs.js
+++ app/config/apidocs.js
@@ -2,6 +2,14 @@ const swaggerUi = require('swagger-ui-express');
 const swaggerDocument = require('../apidocs/swagger.json');
 
 export default (app) => {
+  // specify host and scheme to swagger depending on the environment
+  if (process.env.NODE_ENV === 'prod') {
+    swaggerDocument.host = process.env.API_URI_PROD;
+    swaggerDocument.schemes = ['https'];
+  } else {
+    swaggerDocument.host = process.env.API_URI_DEV;
+  }
+
   // Load the swagger ui
   app.use('/api/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 };
